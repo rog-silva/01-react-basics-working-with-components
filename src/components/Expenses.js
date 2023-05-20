7ff71381c5422afdbc6cbdb7a2a1909fabf488a1
Roger Silva <roger.silva@live.cl>
2023-05-20 15:23:14 +0200

    Create Expenses.js Create Expenses.css Create Card.js Create Card.css

---
+++ src/components/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.elements[0].title}
+        amount={props.elements[0].amount}
+        date={props.elements[0].date}
+      />
+      <ExpenseItem
+        title={props.elements[1].title}
+        amount={props.elements[1].amount}
+        date={props.elements[1].date}
+      />
+      <ExpenseItem
+        title={props.elements[2].title}
+        amount={props.elements[2].amount}
+        date={props.elements[2].date}
+      />
+      <ExpenseItem
+        title={props.elements[3].title}
+        amount={props.elements[3].amount}
+        date={props.elements[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
