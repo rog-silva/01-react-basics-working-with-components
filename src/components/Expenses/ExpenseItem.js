290afe4d8e144c93b0ce102cd3b6479b70c53653
Roger Silva <roger.silva@live.cl>
2023-05-24 09:01:23 +0200

    Create click event handler for button in ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Updated!";
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">€ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change me!</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
