49501f5585481e5f0e1d9302f61e9ce2ea1aa4ed
Roger Silva <roger.silva@live.cl>
2023-06-28 23:49:40 +0200

    Deleted testing code in ExpenseItem.js Added a comment of derive or compute values in Expenses.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("updated, ExpenseItem.js L11");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">€ {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change me!</button>
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,6 +8,28 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2019");
 
+  // you can derive or compute values with and "let" element, so we not create another
+  // useState that depends of the previous useState.
+  // For example if we want to add a text saying that the information for x year is hidde,
+  // we can do it with let
+  //
+  // let filterInfoText = "2020, 2021, 2022 & 2023"
+  // ...
+  // if (filteredYear === "2019") {
+  //   filterInfoText = "2020, 2021, 2022 & 2023"
+  // } else if (filteredYear === "2020") {
+  //   filterInfoText = "2019, 2021, 2022 & 2023"
+  // }  else if (filteredYear === "2021") {
+  //   filterInfoText = "2019, 2020, 2022 & 2023"
+  // } else if (filteredYear === "2022") {
+  //   filterInfoText = "2019, 2020, 2021 & 2023"
+  // } else {
+  //   filterInfoText = "2019, 2020, 2021 & 2022"
+  // }
+  // ...
+  // then just add the var filterInfoText in our jsx, like this:
+  // <p>The information for the years {filterInfoText} is hidden</p>
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
